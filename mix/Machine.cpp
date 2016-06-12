999884ca61689364873d4fbee6eba6f37b2168fe
Sean Vincent <sean@searchspring.com>
2016-06-11 19:16:09 -0600

    Removes poorly thought out functions

--- mix/Machine.cpp
+++ mix/Machine.cpp
@@ -4,167 +4,112 @@
 
 namespace MIX
 {
-    /*
-    * Construct a MIX machine.
-    * All registers are cleared to +0.
-    * All memory words are also cleared to +0.
-    */
-    Machine::Machine()
-        : _overflow{Bit::On},
-          _comparison_indicator{Comparison::Equal},
-          _accumulator{},
-          _extension{},
-          _jump{},
-          _index_registers(num_index_registers),
-          _memory(memory_size),
-          _operation_table{}
-    {
-        setup_operation_table();
-    }
+	// Load op code range.
+	const unsigned int load_lower{8};
+	const unsigned int load_upper{23};
 
-    /*
-    * Adds pointers to all machine operations to operation table.
-    */
-    void Machine::setup_operation_table()
-    {
-        _operation_table.push_back(&Machine::lda);
-    }
+	/*
+	* Construct a MIX machine.
+	* All registers are cleared to +0.
+	* All memory words are also cleared to +0.
+	*/
+	Machine::Machine()
+		: _overflow{Bit::On},
+		  _comparison_indicator{Comparison::Equal},
+		  _accumulator{},
+		  _extension{},
+		  _jump{},
+		  _index_registers(num_index_registers),
+		  _memory(memory_size)
+	{
+	}
 
-    /*
-    * Execute the given instruction word.
-    */
-    void Machine::execute_instruction(const Word& instruction)
-    {
-        operation op = _operation_table[read_op_code(instruction)];
-        (this->*op)(instruction);
-    }
+	/*
+	* Return the index register at the given index.
+	* Index is 1 based because register names are [I1, I6].
+	* Register names are defined by Knuth, Volume 1, Chapter 3.1
+	*/
+	const Half_word& Machine::index_register(int index) const
+	{
+		if(index < 1 || num_index_registers < index)
+			throw std::invalid_argument{"Index register index out of bounds"};
+		return _index_registers[index - 1];
+	}
 
-    /*
-    * Get the contents of memory at the given address.
-    * Only those bytes specified by the given field are copied
-    * to the word returned.
-    */
-    const Word Machine::contents(unsigned int address,
-                                 const Instruction::Field& field) const
-    {
-        const Word& mem = memory(address);
-        return get_bytes(mem, field);
-    }
-
-    /*
-    * Get the bytes in the given field from the given word, right shifted.
-    */
-    const Word Machine::get_bytes(const Word& word,
-                                  const Instruction::Field& field) const
-    {
-        Word destination_word{};
-        int destination_byte = Word::num_bytes;
-        for(int i = field.right; i >= field.left && i > 0; --i)
-        {
-            destination_word.byte(destination_byte--) = word.byte(i);
-        }
-        if(field.left == 0)
-            destination_word.sign(word.sign());
-        return destination_word;
-    }
-
-    /*
-    * Return the index register at the given index.
-    * Index is 1 based because register names are [I1, I6].
-    * Register names are defined by Knuth, Volume 1, Chapter 3.1
-    */
-    const Half_word& Machine::index_register(int index) const
-    {
-        if(index < 1 || num_index_registers < index)
-            throw std::invalid_argument{"Index register index out of bounds"};
-        return _index_registers[index - 1];
-    }
-
-    /*
-    * Return the index register at the given index.
-    * Allows the index register to be modified.
-    * Index is 1 based because register names are [I1, I6].
-    * Register names are defined by Knuth, Volum 1, Chapter 3.1
-    */
-    Half_word& Machine::index_register(int index)
-    {
-        if(index < 1 || num_index_registers < index)
-            throw std::invalid_argument{"Index register invalid"};
-        return _index_registers[index - 1];
-    }
+	/*
+	* Return the index register at the given index.
+	* Allows the index register to be modified.
+	* Index is 1 based because register names are [I1, I6].
+	* Register names are defined by Knuth, Volum 1, Chapter 3.1
+	*/
+	Half_word& Machine::index_register(int index)
+	{
+		if(index < 1 || num_index_registers < index)
+			throw std::invalid_argument{"Index register invalid"};
+		return _index_registers[index - 1];
+	}
 
 
-    /*
-    * Return the memory word at the given index.
-    */
-    const Word& Machine::memory(int address) const
-    {
-        if(address < 0 || memory_size <= address)
-            throw std::invalid_argument{"Memory index out of bounds"};
-        return _memory[address];
-    }
+	/*
+	* Return the memory word at the given index.
+	*/
+	const Word& Machine::memory(int address) const
+	{
+		if(address < 0 || memory_size <= address)
+			throw std::invalid_argument{"Memory index out of bounds"};
+		return _memory[address];
+	}
 
-    /*
-    * Return a reference to the memory word at the given address.
-    * Allows the reference to be modified.
-    */
-    Word& Machine::memory(int address)
-    {
-        if(address < 0 || memory_size <= address)
-            throw std::invalid_argument{"Memory address out of bounds"};
-        return _memory[address];
-    }
+	/*
+	* Return a reference to the memory word at the given address.
+	* Allows the reference to be modified.
+	*/
+	Word& Machine::memory(int address)
+	{
+		if(address < 0 || memory_size <= address)
+			throw std::invalid_argument{"Memory address out of bounds"};
+		return _memory[address];
+	}
 
-    /*
-    * Return the address specification of the given instruction word.
-    * The address is offset by the contents of the index register
-    * specified by the instruction's index specification on every instruction.
-    * If the index specification is 0, there is no offset.
-    */
-    const int Machine::read_address(const Word& instruction) const
-    {
-        int a{address(instruction)};
-        const unsigned int i{read_index(instruction)};
-        if(i > 0)
-            a += address(index_register(i));
-        return a;
-    }
+	/*
+	* Return the address specification of the given instruction word.
+	* The address is offset by the contents of the index register
+	* specified by the instruction's index specification on every instruction.
+	* If the index specification is 0, there is no offset.
+	*/
+	const int Machine::read_address(const Word& instruction) const
+	{
+		int a{address(instruction)};
+		const unsigned int i{read_index(instruction)};
+		if(i > 0)
+			a += address(index_register(i));
+		return a;
+	}
 
-    /*
-    * Return the index specification of the given instruction word.
-    */
-    const unsigned int Machine::read_index(const Word& instruction) const
-    {
-        const unsigned int i{instruction.byte(Instruction::index)};
-        if(i < 0 || num_index_registers < i)
-            throw std::invalid_argument{"Index specification invalid"};
-        return i;
-    }
+	/*
+	* Return the index specification of the given instruction word.
+	*/
+	const unsigned int Machine::read_index(const Word& instruction) const
+	{
+		const unsigned int i{instruction.byte(Instruction::index)};
+		if(i < 0 || num_index_registers < i)
+			throw std::invalid_argument{"Index specification invalid"};
+		return i;
+	}
 
-    /*
-    * Return the field specification of the given instruction word.
-    */
-    const Instruction::Field Machine::read_field(const Word& instruction) const
-    {
-        return Instruction::to_field_spec(
-            instruction.byte(Instruction::modification)
-        );
-    }
+	/*
+	* Return the field specification of the given instruction word.
+	*/
+	const Field Machine::read_field(const Word& instruction) const
+	{
+		return to_field(instruction.byte(Instruction::modification));
+	}
 
-    /*
-    * Read the operation code part of the given instruction word.
-    */
-    const unsigned int Machine::read_op_code(const Word& instruction) const
-    {
-        return instruction.byte(Instruction::op_code);
-    }
-
-    /*
-    * Load the accumulator according to the given instruction.
-    */
-    void Machine::lda(const Word& instruction)
-    {
-
-    }
+	/*
+	* Read the operation code part of the given instruction word.
+	*/
+	const unsigned int Machine::read_op_code(const Word& instruction) const
+	{
+		return instruction.byte(Instruction::op_code);
+	}
 }
-
--- mix/Machine.h
+++ mix/Machine.h
@@ -12,67 +12,47 @@
 
 namespace MIX
 {
-    class Machine
-    {
-    public:
-        // Constants.
-        static const unsigned int num_index_registers{6};
-        static const unsigned int memory_size{4000};
+	class Machine
+	{
+	public:
+		// Constants.
+		static const unsigned int num_index_registers{6};
+		static const unsigned int memory_size{4000};
 
-        // Constructors.
-        Machine();
-        Machine(const Machine&) = delete;
-        Machine(Machine&&) = delete;
+		// Constructors.
+		Machine();
+		Machine(const Machine&) = delete;
+		Machine(Machine&&) = delete;
 
+		// Accessors.
+		const Word& accumulator() const { return _accumulator; }
+		const Word& extension() const { return _extension; }
+		const Half_word& jump_register() const { return _jump; }
+		const Half_word& index_register(int) const;
+		const Word& memory(int) const;
 
-        // Execute the instruction.
-        void execute_instruction(const Word&);
-        // Get the contents of memory at the given address.
-        const Word contents(unsigned int, const Instruction::Field&) const;
-        // Get the bytes from the given word in the field, right shifted.
-        const Word get_bytes(const Word&, const Instruction::Field&) const;
+		// Mutators.
+		void accumulator(const Word& a) { _accumulator = a; }
+		void extension(const Word& x) { _extension = x; }
+		void jump_register(const Half_word& j) { _jump = j; }
+		Half_word& index_register(int);
+		Word& memory(int);
 
+		// Methods to read parts of instructions.
+		const int read_address(const Word&) const;
+		const unsigned int read_index(const Word&) const;
+		const unsigned int read_modification(const Word&) const;
+		const Field read_field(const Word&) const;
+		const unsigned int read_op_code(const Word&) const;
 
-        // Accessors.
-        const Word& accumulator() const { return _accumulator; }
-        const Word& extension() const { return _extension; }
-        const Half_word& jump_register() const { return _jump; }
-        const Half_word& index_register(int) const;
-        const Word& memory(int) const;
-
-        // Mutators.
-        void accumulator(const Word& a) { _accumulator = a; }
-        void extension(const Word& x) { _extension = x; }
-        void jump_register(const Half_word& j) { _jump = j; }
-        Half_word& index_register(int);
-        Word& memory(int);
-
-        // Methods to read parts of instructions.
-        const int read_address(const Word&) const;
-        const unsigned int read_index(const Word&) const;
-        const unsigned int read_modification(const Word&) const;
-        const Instruction::Field read_field(const Word&) const;
-        const unsigned int read_op_code(const Word&) const;
-
-    private:
-        // Pointer to a machine operation.
-        using operation = void(Machine::*)(const Word&);
-
-        Bit _overflow;                            // Overflow bit (on/off).
-        Comparison _comparison_indicator;         // Comparison flag.
-        Word _accumulator;                        // Accumulator register (A).
-        Word _extension;                          // Extension register (X).
-        Half_word _jump;                          // Jump register (J).
-        std::vector<Half_word> _index_registers;  // Index registers (I1-I6).
-        std::vector<Word> _memory;                // 4000 word memory (0-3999).
-        std::vector<operation> _operation_table;  // Operation dispatch table.
-
-        // Set up the operation table.
-        void setup_operation_table();
-
-        // Machine operations.
-        void lda(const Word&);
-    };
+	private:
+		Bit _overflow;								// Overflow bit (on/off).
+		Comparison _comparison_indicator;			// Comparison flag.
+		Word _accumulator;							// Accumulator register (A).
+		Word _extension;							// Extension register (X).
+		Half_word _jump;							// Jump register (J).
+		std::vector<Half_word> _index_registers;	// Index registers.
+		std::vector<Word> _memory;					// Machine memory.
+	};
 }
 #endif
-
