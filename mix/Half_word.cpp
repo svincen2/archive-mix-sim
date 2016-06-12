53cb5281caf27ed3cce245340becdb12e1ac055e
Sean Vincent <sean@searchspring.com>
2016-06-11 19:21:12 -0600

    Changes spaces to tabs

--- mix/Half_word.cpp
+++ mix/Half_word.cpp
@@ -2,14 +2,14 @@
 
 namespace MIX
 {
-    /*
-    * Construct a half word with the given values.
-    */
-    Half_word::Half_word(Sign s, Byte b1, Byte b2)
-        : Basic_word{}
-    {
-        sign(s);
-        byte(1) = b1;
-        byte(2) = b2;
-    }
+	/*
+	* Construct a half word with the given values.
+	*/
+	Half_word::Half_word(Sign s, Byte b1, Byte b2)
+		: Basic_word{}
+	{
+		sign(s);
+		byte(1) = b1;
+		byte(2) = b2;
+	}
 }
--- mix/Half_word.h
+++ mix/Half_word.h
@@ -5,15 +5,14 @@
 
 namespace MIX
 {
-    // A half word has a sign and 2 bytes.
-    class Half_word : public Basic_word<2>
-    {
-    public:
-        // Parameterized constructor with default values.
-        Half_word(Sign s = Sign::Plus,
-                  Byte b1 = 0,
-                  Byte b2 = 0);
-    };
+	// A half word has a sign and 2 bytes.
+	class Half_word : public Basic_word<2>
+	{
+	public:
+		// Parameterized constructor with default values.
+		Half_word(Sign s = Sign::Plus,
+				  Byte b1 = 0,
+				  Byte b2 = 0);
+	};
 }
 #endif
-
