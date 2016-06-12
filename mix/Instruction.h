f50a84c530564a2253e8bc3e1e16d7891486ac18
Sean Vincent <sean@searchspring.com>
2016-06-11 19:22:37 -0600

    Changes spaces to tabs and adds address specification

--- mix/Instruction.h
+++ mix/Instruction.h
@@ -3,15 +3,16 @@
 
 namespace MIX
 {
-    /*
-    * Constants identifying parts of an instruction word.
-    */
-    namespace Instruction
-    {
-        const unsigned int index{3};
-        const unsigned int modification{4};
-        const unsigned int op_code{5};
-    }
+	/*
+	* Constants identifying parts of an instruction word.
+	*/
+	namespace Instruction
+	{
+		const unsigned int address_msb{1};
+		const unsigned int address_lsb{2};
+		const unsigned int index{3};
+		const unsigned int modification{4};
+		const unsigned int op_code{5};
+	}
 }
 #endif
-
