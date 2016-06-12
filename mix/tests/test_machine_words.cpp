357d386ded12c4523fc71e3e7c32f917a458a284
Sean Vincent <sean@searchspring.com>
2016-06-12 16:55:55 -0600

    Changes spaces to tabs

--- mix/tests/test_machine_words.cpp
+++ mix/tests/test_machine_words.cpp
@@ -6,76 +6,75 @@ using namespace MIX;
 
 SCENARIO("Invalid byte indexing")
 {
-  GIVEN("A Half_word")
-  {
-    Half_word hw{};
-    WHEN("Byte index is less than 0")
-    {
-      int index{-1};
-      THEN("Invalid argument is thrown")
-      {
-        REQUIRE_THROWS_AS(hw.byte(index), std::invalid_argument);
-      }
-    }
-    WHEN("Byte index is greater than num_bytes")
-    {
-      int index{Half_word::num_bytes + 1};
-      THEN("Invalid argument is thrown")
-      {
-        REQUIRE_THROWS_AS(hw.byte(index), std::invalid_argument);
-      }
-    }
-  }
+	GIVEN("A Half_word")
+	{
+		Half_word hw{};
+		WHEN("Byte index is less than 0")
+		{
+			int index{-1};
+			THEN("Invalid argument is thrown")
+			{
+				REQUIRE_THROWS_AS(hw.byte(index), std::invalid_argument);
+			}
+		}
+		WHEN("Byte index is greater than num_bytes")
+		{
+			int index{Half_word::num_bytes + 1};
+			THEN("Invalid argument is thrown")
+			{
+				REQUIRE_THROWS_AS(hw.byte(index), std::invalid_argument);
+			}
+		}
+	}
 
-  GIVEN("A Word")
-  {
-    Word w{};
-    WHEN("Byte index is less than 0")
-    {
-      int index{-1};
-      THEN("Invalid argument is thrown")
-      {
-        REQUIRE_THROWS_AS(w.byte(index), std::invalid_argument);
-      }
-    }
-    WHEN("Byte index is greater than num_bytes")
-    {
-      int index{Word::num_bytes + 1};
-      THEN("Invalid argument is thrown")
-      {
-        REQUIRE_THROWS_AS(w.byte(index), std::invalid_argument);
-      }
-    }
-  }
+	GIVEN("A Word")
+	{
+		Word w{};
+		WHEN("Byte index is less than 0")
+		{
+			int index{-1};
+			THEN("Invalid argument is thrown")
+			{
+				REQUIRE_THROWS_AS(w.byte(index), std::invalid_argument);
+			}
+		}
+		WHEN("Byte index is greater than num_bytes")
+		{
+			int index{Word::num_bytes + 1};
+			THEN("Invalid argument is thrown")
+			{
+				REQUIRE_THROWS_AS(w.byte(index), std::invalid_argument);
+			}
+		}
+	}
 }
 
 
 SCENARIO("Setting the sign to an illegal value")
 {
-  GIVEN("A Half_word")
-  {
-    Half_word hw{};
-    WHEN("Setting sign to invalid value")
-    {
-      THEN("Invalid argument is thrown")
-      {
-        REQUIRE_THROWS_AS(hw.sign(static_cast<Sign>(-1)),
-                          std::invalid_argument);
-      }
-    }
-  }
+	GIVEN("A Half_word")
+	{
+		Half_word hw{};
+		WHEN("Setting sign to invalid value")
+		{
+			THEN("Invalid argument is thrown")
+			{
+				REQUIRE_THROWS_AS(hw.sign(static_cast<Sign>(-1)),
+								  std::invalid_argument);
+			}
+		}
+	}
 
-  GIVEN("A Word")
-  {
-    Word w{};
-    WHEN("Setting sign to invalid value")
-    {
-      THEN("Invalid argument is thrown")
-      {
-        REQUIRE_THROWS_AS(w.sign(static_cast<Sign>(-1)),
-                          std::invalid_argument);
-      }
-    }
-  }
+	GIVEN("A Word")
+	{
+		Word w{};
+		WHEN("Setting sign to invalid value")
+		{
+			THEN("Invalid argument is thrown")
+			{
+				REQUIRE_THROWS_AS(w.sign(static_cast<Sign>(-1)),
+								  std::invalid_argument);
+			}
+		}
+	}
 }
-
