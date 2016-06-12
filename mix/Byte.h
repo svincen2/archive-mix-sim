4886bafabf311081d455efaa6bb038f372dc5453
Sean Vincent <sean@searchspring.com>
2016-06-11 19:20:05 -0600

    Changes spaces to tabs

--- mix/Byte.h
+++ mix/Byte.h
@@ -5,14 +5,13 @@
 
 namespace MIX
 {
-  // Alias for a Byte.
-  using Byte = unsigned char;
+	// Alias for a Byte.
+	using Byte = unsigned char;
 
-  // The number of bits in a byte.
-  const int byte_size{6};
+	// The number of bits in a byte.
+	const int byte_size{6};
 
-  // Mask to read only byte_size number of bits from a Byte.
-  const int bit_mask{static_cast<int>(std::pow(2, byte_size)) - 1};
+	// Mask to read only byte_size number of bits from a Byte.
+	const int bit_mask{static_cast<int>(std::pow(2, byte_size)) - 1};
 }
 #endif
-
