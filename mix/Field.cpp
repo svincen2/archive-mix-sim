795d769c7df25721ddf8ebf4a416e649d0596524
Sean Vincent <sean@searchspring.com>
2016-06-11 19:04:19 -0600

    Removes Instruction namespace

--- mix/Field.cpp
+++ mix/Field.cpp
@@ -2,48 +2,45 @@
 
 namespace MIX
 {
-    namespace Instruction
-    {
-        // Field specification's left part scaling factor.
-        const unsigned int left_scale{8};
+	// Field specification's left part scaling factor.
+	const unsigned int left_scale{8};
 
-        /*
-        * Construct the default field specification of (0:5).
-        */
-        Field::Field() : left{0}, right{5}
-        {
-        }
+	/*
+	* Construct the default field specification of (0:5).
+	*/
+	Field::Field() : left{0}, right{5}
+	{
+	}
 
-        /*
-        * Construct a field with the given left and right values.
-        */
-        Field::Field(unsigned int l, unsigned int r) : left{l}, right{r}
-        {
-        }
+	/*
+	* Construct a field with the given left and right values.
+	*/
+	Field::Field(unsigned int l, unsigned int r) : left{l}, right{r}
+	{
+	}
 
-        /*
-        * Copy the given field.
-        */
-        Field::Field(const Field& f) : left{f.left}, right{f.right}
-        {
-        }
+	/*
+	* Copy the given field.
+	*/
+	Field::Field(const Field& f) : left{f.left}, right{f.right}
+	{
+	}
 
-        /*
-        * Convert the given number into a field specification.
-        */
-        Field to_field_spec(const unsigned int ui)
-        {
-            unsigned int left{ui / left_scale};
-            unsigned int right{ui % left_scale};
-            return Field{left, right};
-        }
+	/*
+	* Convert the given number into a field specification.
+	*/
+	Field to_field(const unsigned int ui)
+	{
+		unsigned int left{ui / left_scale};
+		unsigned int right{ui % left_scale};
+		return Field{left, right};
+	}
 
-        /*
-        * Convert the given field specification into an unsigned int.
-        */
-        Byte to_byte(const Field& fs)
-        {
-            return static_cast<Byte>(fs.left * left_scale + fs.right);
-        }
-    }
+	/*
+	* Convert the given field specification into an unsigned int.
+	*/
+	Byte to_byte(const Field& fs)
+	{
+		return static_cast<Byte>(fs.left * left_scale + fs.right);
+	}
 }
--- mix/Field.h
+++ mix/Field.h
@@ -1,36 +1,32 @@
-#ifndef MIX_INSTRUCTION_FIELD_H
-#define MIX_INSTRUCTION_FIELD_H
+#ifndef MIX_FIELD_H
+#define MIX_FIELD_H
 
 #include "Byte.h"
 
 namespace MIX
 {
-    namespace Instruction
-    {
-        /*
-        * A MIX machine instruction field specification.
-        * Specifies the bytes of a word to be utilized in an instruction.
-        */
-        struct Field
-        {
-            unsigned int left;
-            unsigned int right;
+	/*
+	* A MIX machine instruction field specification.
+	* Specifies the bytes of a word to be utilized in an instruction.
+	*/
+	struct Field
+	{
+		unsigned int left;
+		unsigned int right;
 
-            // Constructors.
-            Field();
-            Field(unsigned int, unsigned int);
-            Field(const Field&);
+		// Constructors.
+		Field();
+		Field(unsigned int, unsigned int);
+		Field(const Field&);
 
-            // Number of bytes in range specified by field.
-            unsigned int range_size() { return right - left + 1; }
-        };
+		// Number of bytes in range specified by field.
+		unsigned int range_size() { return right - left + 1; }
+	};
 
-        // Convert a number to a field specification.
-        Field to_field_spec(const unsigned int);
+	// Convert a number to a field specification.
+	Field to_field(const unsigned int);
 
-        // Convert a field specification to a number.
-        Byte to_byte(const Field&);
-    }
+	// Convert a field specification to a number.
+	Byte to_byte(const Field&);
 }
 #endif
-
