1ce4391708547a4b055f9cc6403f6d5271334ba5
Sean Vincent <sean@searchspring.com>
2016-06-25 21:54:06 -0600

    Adds creation of mix machine and captures machine exit code

--- machine/main.cpp
+++ machine/main.cpp
@@ -5,6 +5,15 @@
 #include <vector>
 
 /*
+* Print the given arguments to std::cout.
+*/
+void print_args(std::vector<std::string>& args) {
+	for (auto p = args.begin(); p != args.end(); ++p) {
+		std::cout << "arg: " << *p << '\n';
+	}
+}
+
+/*
 * Main entry point.
 * Start mix machine and pass it the command line arguments.
 * Parameters:
@@ -13,10 +22,15 @@
 */
 int main(int argc, char* argv[])
 {
+	int exit_code{0};
+
 	std::vector<std::string> args{console::get_args(argc, argv)};
-	for (auto p = args.begin(); p != args.end(); ++p) {
-		std::cout << "arg: " << *p << '\n';
-	}
-	std::cout << "exit: 0\n";
-	return 0;
+	print_args(args);
+
+	mix::Machine machine{};
+	exit_code = machine.run(args);
+
+	std::cout << "exit: " << exit_code << '\n';
+	return exit_code;
 }
+
