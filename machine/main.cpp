a1da7177c8938109ca0eb13fa90ea800811a35fe
Sean Vincent <sean@searchspring.com>
2016-07-06 20:43:38 -0600

    Adds run(), and adds exception handling

--- machine/main.cpp
+++ machine/main.cpp
@@ -14,8 +14,23 @@ void print_args(std::vector<std::string>& args) {
 }
 
 /*
-* Main entry point.
 * Start mix machine and pass it the command line arguments.
+* Parameters:
+*	argc - Number of command line arguments.
+*	argv - Command line arguments.
+*/
+int run(int argc, char* argv[])
+{
+	std::vector<std::string> args{console::get_args(argc, argv)};
+	print_args(args);
+
+	mix::Machine machine{};
+	return machine.run(args);
+}
+
+/*
+* Main entry point.
+* Calls run() and catches exceptions.
 * Parameters:
 *	argc - Number of command line arguments.
 *	argv - Command line arguments.
@@ -24,11 +39,13 @@ int main(int argc, char* argv[])
 {
 	int exit_code{0};
 
-	std::vector<std::string> args{console::get_args(argc, argv)};
-	print_args(args);
-
-	mix::Machine machine{};
-	exit_code = machine.run(args);
+	try {
+		exit_code = run(argc, argv);
+	}
+	catch(std::exception& e) {
+		std::cout << e.what() << std::endl;
+		exit_code = -1;
+	}
 
 	std::cout << "exit: " << exit_code << '\n';
 	return exit_code;
