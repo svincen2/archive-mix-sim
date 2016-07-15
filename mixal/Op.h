39b0253548bf3d67533923626990cb4f7574e224
Sean Vincent <vincent.sean@hotmail.com>
2016-07-14 18:14:22 -0600

    Adds mixal operation codes enum

---
+++ mixal/Op.h
@@ -0,0 +1,23 @@
+#ifndef MIX_MIXAL_OP_H
+#define MIX_MIXAL_OP_H
+
+#include "../machine/Byte.h"
+
+namespace mixal
+{
+	// Mix assembly language op codes.
+	enum Op : Byte
+	{
+		// Load operations.
+		LDA = 8,
+		LD1, LD2, LD3, LD4, LD5, LD6,
+		LDX,
+
+		// Load negative operations.
+		LDAN,
+		LD1N, LD2N, LD3N, LD4N, LD5N, LD6N,
+		LDXN
+	};
+}
+#endif
+
