8ab46e58e10904d524db4cb0232e5f90d50a6acd
Sean Vincent <vincent.sean@hotmail.com>
2016-07-16 11:34:26 -0600

    Refactors magic numbers, replacing with address field spec constant

--- machine/Machine.cpp
+++ machine/Machine.cpp
@@ -113,11 +113,11 @@ namespace mix
 	*/
 	int Machine::read_address(const Word& instruction) const
 	{
-		int address{instruction.to_int(0, 2)};
+		int address{instruction.to_int(Field_spec::ADDRESS)};
 		int index_spec{instruction.byte(3)};
 		if (index_spec != 0) {
 			const Half_word& index_reg{index_register(index_spec)};
-			address += index_reg.to_int(0, 2);
+			address += index_reg.to_int(Field_spec::ADDRESS);
 		}
 		return address;
 	}
