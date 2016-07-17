575632d35f46c8b331f0b7d6d1d0195378ac0cf2
Sean Vincent <vincent.sean@hotmail.com>
2016-07-16 22:05:45 -0600

    Adds tests for Instruction header functions

---
+++ machine/tests/Instruction_test.cpp
@@ -0,0 +1,46 @@
+#include "catch.hpp"
+#include "../Instruction.h"
+#include "../Word.h"
+
+using namespace mix;
+
+SCENARIO("Getting parts of an instruction")
+{
+	GIVEN("An instruction")
+	{
+		Word inst{Sign::Plus, {1, 2, 3, 4, 5}};
+		WHEN("Getting the address")
+		{
+			int address{get_address(inst)};
+			THEN("The address is correct")
+			{
+				REQUIRE(address == 66);
+			}
+		}
+		WHEN("Getting the index specification")
+		{
+			int index_spec{get_index_spec(inst)};
+			THEN("The index spec is correct")
+			{
+				REQUIRE(index_spec == 3);
+			}
+		}
+		WHEN("Getting the field spec")
+		{
+			Field_spec fs{get_field_spec(inst)};
+			THEN("The field spec is correct")
+			{
+				REQUIRE(fs.left == 0);
+				REQUIRE(fs.right == 4);
+			}
+		}
+		WHEN("Getting the op code")
+		{
+			Byte op{get_op_code(inst)};
+			THEN("The op code is correct")
+			{
+				REQUIRE(op == 5);
+			}
+		}
+	}
+}
