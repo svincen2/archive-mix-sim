6d166743a1c4a556c07f536fe2c1be1dd9d439a1
Sean Vincent <vincent.sean@hotmail.com>
2016-07-16 22:05:15 -0600

    Adds tests for decoding, eqaulity operator, and getting field spec sizes

--- machine/tests/Field_spec_test.cpp
+++ machine/tests/Field_spec_test.cpp
@@ -53,3 +53,78 @@ SCENARIO("Encoding a field spec")
 		}
 	}
 }
+
+SCENARIO("Decoding encoded field spec")
+{
+	GIVEN("A field spec encoding of 0")
+	{
+		int encoded{0};
+		WHEN("Decoded")
+		{
+			Field_spec fs{decode_field_spec(encoded)};
+			THEN("The default field spec is returned")
+			{
+				REQUIRE(fs == Field_spec::DEFAULT);
+			}
+		}
+	}
+	GIVEN("A field spec encoding of 36")
+	{
+		int encoded{36};
+		WHEN("Decoded")
+		{
+			Field_spec fs{decode_field_spec(encoded)};
+			THEN("A field spec of (4:4) is returned")
+			{
+				Field_spec expected{4, 4};
+				REQUIRE(fs == expected);
+			}
+		}
+	}
+}
+
+SCENARIO("Testing equality")
+{
+	GIVEN("A field specification")
+	{
+		Field_spec fs1{0, 5};
+		WHEN("Compared to a field spec with same left and right values")
+		{
+			Field_spec fs2{0, 5};
+			THEN("Equality operator returns true")
+			{
+				REQUIRE((fs1 == fs2) == true);
+			}
+		}
+		WHEN("Compared to a field spec with different left and right values")
+		{
+			Field_spec fs2{1, 3};
+			THEN("Equality operator return false")
+			{
+				REQUIRE((fs1 == fs2) == false);
+			}
+		}
+	}
+}
+
+SCENARIO("Field_spec sizes")
+{
+	GIVEN("A field spec")
+	{
+		Field_spec fs{0, 5};
+		WHEN("Asked for the size of the field spec")
+		{
+			THEN("The size includes all bytes and the sign, if included")
+			{
+				REQUIRE(fs.size() == 6);
+			}
+		}
+		WHEN("Asked for the bytes of the field spec")
+		{
+			THEN("The size includes only the bytes")
+			{
+				REQUIRE(fs.bytes() == 5);
+			}
+		}
+	}
+}
